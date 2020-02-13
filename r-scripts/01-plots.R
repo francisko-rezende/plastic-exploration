8e30b7d6bc6d169aa2662227064ab44da8305011
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2020-02-13 13:21:18 +0000

    plotted basic versions

--- r-scripts/01-plots.R
+++ r-scripts/01-plots.R
@@ -30,10 +30,40 @@ ggplot2::ggplot(prod, aes(x = Year, y = `Global plastics production (million ton
   geom_line()+
   geom_point()
 
-## per capita plastic waste generation
-ggplot2::ggplot(capita, aes(x = factor_country, y = `Per capita plastic waste (kilograms per person per day)`, fill = group)) +
-  geom_col() +
-  coord_flip() +
-  theme(axis.text.y = element_blank())
-  
 
+# finding countries with pop size similar to fin --------------------------
+
+europe <- gapminder::gapminder %>% 
+  mutate(fin = if_else(country == "Finland", "Finland", "Other"),
+         country = fct_reorder(country, pop)) %>% 
+  filter(year == 2007) %>% 
+  filter(continent == "Europe") %>% 
+  select(country)
+
+europe <-  as.character(europe$country)
+
+## per capita plastic waste generation
+
+capita %>% 
+  filter(Entity %in% europe) %>% 
+  ggplot2::ggplot(aes(x = factor_country, y = `Per capita plastic waste (kilograms per person per day)`, fill = group)) +
+  geom_col() +
+  coord_flip()
+
+# pop_2010 <- read_csv("data/raw/population-figures-by-country-csv_csv.csv") %>% 
+#   pivot_longer(cols = c(-1, -2), names_to = "year", values_to = "pop") %>% 
+#   separate(year, into = c(NA, "year")) %>% 
+#   filter(year == 2010,
+#          pop > 3000000,
+#          pop < 7000000) %>% 
+#   mutate(year = as.numeric(year),
+#          Country = factor(Country),
+#          Country = fct_reorder(Country,  pop),
+#          fin = if_else(Country == "Finland", "Finland", "Other"))
+#   
+#   ggplot(pop_2010, aes(Country, pop, fill = fin)) +
+#   geom_col() +
+#   coord_flip() +
+#   scale_y_continuous(labels = scales::comma)
+# 
+#   
